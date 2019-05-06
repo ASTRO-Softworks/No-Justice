1dbfac21947c30cd884f0c771c981d44931731b9
Anton <larin.an10n@gmail.com>
2019-05-06 21:46:25 +0600

    Strange stuff

---
+++ Assets/CameraFly_test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFly_test : MonoBehaviour
+{
+    public float cameraSpeedMovement = 1f;
+
+    void Update()
+    {
+        Vector2 flyMovement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 flyDirection = flyMovement.normalized;
+        Vector2 flyForce = flyDirection * cameraSpeedMovement;
+        transform.Translate(flyForce * Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/Scripts/Traps/Door.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Collider2D collider;
+    [SerializeField] SpriteRenderer sprite;
+    bool open = false;
+    int cnt = 0;
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    */
+    public void Toggle()
+    {
+        if (cnt > 0)
+        {
+            cnt = 0;
+            collider.isTrigger = true;
+            sprite.color = new Color(1, 1, 1, 0);
+        }
+        else
+        {
+            cnt = 1;
+            collider.isTrigger = false;
+            sprite.color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    public void Open(bool flag)
+    {
+        cnt += flag?1:-1;
+        Debug.Log("Door " + flag);
+        open = cnt > 0;
+        if (open)
+        {
+            collider.isTrigger = true;
+            sprite.color = new Color(1, 1, 1, 0);
+        }
+        else
+        {
+            collider.isTrigger = false;
+            sprite.color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    
+}
