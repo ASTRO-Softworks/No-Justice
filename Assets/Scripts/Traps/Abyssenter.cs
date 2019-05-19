55e791c9280b8e743c5e2db2fd8e3a87079e1e54
frodios <45042658+frodios@users.noreply.github.com>
2019-05-19 17:50:20 +0300

    Add files via upload

---
+++ Assets/Scripts/Traps/Abyssenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abyssenter : MonoBehaviour
+{
+    private AbstractCharacter _ac;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D Player)
+    {
+        if (_ac = Player.gameObject.GetComponent<AbstractCharacter>())
+        {
+            _ac.Die();
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
